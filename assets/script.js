3d4518e703dca67e2e0a89978fddb4a28d0ffff4
Elodie G <elog1998@gmail.com>
2024-12-10 16:33:47 +0100

    Modification du JavaScript avec fonctions pour le changement d'image du carousel et le dot selected

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,48 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
 
-const buttonLeft = document.querySelector('.prev');
-buttonLeft.addEventListener('click', () => {
-	console.log("Le bouton de gauche est cliqué");
+let diapositiveActuelle = 0;
+const carouselSlide = document.querySelector(".banner-img");
+
+function afficherDiapositive () {
+	carouselSlide.src = slides[diapositiveActuelle].image;
+
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot, i) => {
+		if (i === diapositiveActuelle) {
+			dot.classList.add("dot_selected");
+		} else {
+			dot.classList.remove("dot_selected");
+		}
+	})
+}
+
+document.querySelector(".prev").addEventListener("click", function() {
+	diapositiveActuelle = (diapositiveActuelle - 1 + slides.length) % slides.length;
+  	afficherDiapositive();
 })
 
-const buttonRight = document.querySelector('.next');
-buttonRight.addEventListener('click', () => {
-	console.log("Le bouton de droite est cliqué");
+document.querySelector(".next").addEventListener("click", function() {
+	diapositiveActuelle = (diapositiveActuelle + 1 + slides.length) % slides.length;
+  	afficherDiapositive();
 })
+
+afficherDiapositive();
+
 
